f8d69b23dacfa1cac038168c77cf8ce0ffc43528
Prasanth K S <ksprasanthksp10@gmail.com>
2025-01-21 10:06:49 +0530

    Event Handling Example 21.01.2025(FN)

--- seceFrontend/src/components/FunctionalComponents/About.jsx
+++ seceFrontend/src/components/FunctionalComponents/About.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+// Event Handling
 const About = () => {
-    return(
-        <div>
-            <h2>This is About Component.</h2>
-        </div>
-    )
-}
+  var [text, setText] = useState("Vijay");
+  function handleText(e){
+    setText(e.target.value)
+  }
+  return (
+    <div>
+      <h2>This is About Component.</h2>
+      <h2>TextArea:</h2>
+{/* <textarea value={text} onChange={(event=>setText(event.target.value)} /> */}
+      <textarea value={text} onChange={handleText} />
+      <p>The text is {text}</p>
+    </div>
+  );
+};
 export default About;
