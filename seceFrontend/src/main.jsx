e4b9ebdfe49b65b5144668c010bc08422d1d604b
Prasanth K S <ksprasanthksp10@gmail.com>
2025-01-29 19:05:10 +0530

    Vercel Deployment

--- seceFrontend/src/main.jsx
+++ seceFrontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
-import AuthContextProvider from './components/FunctionalComponents/ContextComponents/AuthCOntext'
+import AuthContextProvider from './components/FunctionalComponents/ContextComponents/AuthContext'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
